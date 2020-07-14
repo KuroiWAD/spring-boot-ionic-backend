53f35740a57598cd8cba1189adcaa7ac31667f51
KuroiWAD <daniel-m-df@hotmail.com>
2020-07-14 18:01:59 -0300

    Criada as camadas DAO(repository) e Service para Categoria

---
+++ src/main/java/com/daniel/cursomc/dao/CategoriaDAO.java
@@ -0,0 +1,12 @@
+package com.daniel.cursomc.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daniel.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaDAO extends JpaRepository<Categoria, Integer>{
+	
+
+}
---
+++ src/main/java/com/daniel/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.daniel.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daniel.cursomc.dao.CategoriaDAO;
+import com.daniel.cursomc.domain.Categoria;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaDAO dao;
+	
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = dao.findById(id);
+		return obj.orElse(null);
+		} 
+
+}
