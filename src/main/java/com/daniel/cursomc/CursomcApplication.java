922b1dbba5369f9390ae59a82508069498765b42
KuroiWAD <daniel-m-df@hotmail.com>
2021-01-31 11:42:21 -0300

    ItemPedido e ItemPedidoPK (PrimaryKey)

--- src/main/java/com/daniel/cursomc/CursomcApplication.java
+++ src/main/java/com/daniel/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.daniel.cursomc.dao.CidadeDAO;
 import com.daniel.cursomc.dao.ClienteDAO;
 import com.daniel.cursomc.dao.EnderecoDAO;
 import com.daniel.cursomc.dao.EstadoDAO;
+import com.daniel.cursomc.dao.ItemPedidoDAO;
 import com.daniel.cursomc.dao.PagamentoDAO;
 import com.daniel.cursomc.dao.PedidoDAO;
 import com.daniel.cursomc.dao.ProdutoDAO;
@@ -21,6 +22,7 @@ import com.daniel.cursomc.domain.Cidade;
 import com.daniel.cursomc.domain.Cliente;
 import com.daniel.cursomc.domain.Endereco;
 import com.daniel.cursomc.domain.Estado;
+import com.daniel.cursomc.domain.ItemPedido;
 import com.daniel.cursomc.domain.Pagamento;
 import com.daniel.cursomc.domain.PagamentoComBoleto;
 import com.daniel.cursomc.domain.PagamentoComCartao;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PagamentoDAO pagamentoDAO;
 	@Autowired
 	private PedidoDAO pedidoDAO;
+	@Autowired
+	private ItemPedidoDAO itemPedidoDAO;
 
 	
 	public static void main(String[] args) {
@@ -118,6 +122,18 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		pagamentoDAO.saveAll(Arrays.asList(pagto1,pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoDAO.saveAll(Arrays.asList(ip1,ip2,ip3));
 		
 	}
 
